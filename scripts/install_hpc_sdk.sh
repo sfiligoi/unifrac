3390b4f28bcc9b59b9424bd0bf8b2bb98fad08ed
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-01-21 12:59:10 -0800

    Make it little more generic

--- scripts/install_hpc_sdk.sh
+++ scripts/install_hpc_sdk.sh
@@ -6,11 +6,23 @@
 #
 
 # Create GCC symbolic links
+# since NVIDIA HPC SDK does not use the env variables
+if [ "x${GCC}" == "x" ]; then
+  echo "ERROR: GCC not defined"
+  exit 1
+fi
+
+# usually $CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-
+EXE_PREFIX=${GCC%%*(gcc)}
+
+echo "GCC pointing to ${EXE_PREFIX}gcc"
+ls -l ${EXE_PREFIX}gcc
+
 mkdir conda_nv_bins
 (cd conda_nv_bins && for f in \
   ar as c++ cc cpp g++ gcc ld nm ranlib strip; \
   do \
-    ln -s $CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-${f} ${f}; \
+    ln -s ${EXE_PREFIX}${f} ${f}; \
   done )
 
 # create helper scripts
@@ -34,7 +46,7 @@ sed -i -e "s#PATH=/#PATH=$PWD/conda_nv_bins:/#g" \
 
 # Install the NVIDIA HPC SDK
 
-# This llink may need to be updated, as new compiler versions are released
+# This link may need to be updated, as new compiler versions are released
 wget https://developer.download.nvidia.com/hpc-sdk/20.11/nvhpc_2020_2011_Linux_x86_64_cuda_11.1.tar.gz
 tar xpzf nvhpc_*.tar.gz
 rm -f nvhpc_*.tar.gz
