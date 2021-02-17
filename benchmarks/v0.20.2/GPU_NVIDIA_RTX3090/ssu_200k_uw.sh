e6e40dbbd0eb97af77bfd7f8f4a9967281ed2c2e
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-02-17 13:36:45 -0800

    Add more test results

---
+++ benchmarks/v0.20.2/GPU_NVIDIA_RTX3090/ssu_200k_uw.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+hostname
+lscpu
+date
+nvidia-smi
+
+export ACC_DEVICE_NUM=0
+export OMP_NUM_THREADS=8
+
+source ~/.bashrc
+conda activate unifrac-gpu
+
+md5sum unifrac_200000.biom
+md5sum unifrac_200000.tre
+
+echo "===="
+ssu -i unifrac_200000.biom -t unifrac_200000.tre -m unweighted -o unifrac.tmp --mode partial-report
+echo "=== unweighted 100k/200k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+s1=0
+for ((i=0; $i<16; i+=1)); do
+  let s2=s1+6250
+  echo "`date` ssu --start $s1 --stop $s2"
+  taskset -c 0-7 time ssu -m unweighted -i unifrac_200000.biom -t unifrac_200000.tre -o unifrac.tmp --mode partial --start $s1 --stop $s2
+  rm -fr unifrac.tmp
+  s1=$s2
+done
+t2=`date +%s`
+let dt=t2-t1
+echo "=== unweighted time: $dt ==="
+
+t1=`date +%s`
+s1=0
+for ((i=0; $i<16; i+=1)); do
+  let s2=s1+6250
+  echo "`date` ssu --start $s1 --stop $s2"
+  taskset -c 0-7 time ssu -m unweighted -f -i unifrac_200000.biom -t unifrac_200000.tre -o unifrac.tmp --mode partial --start $s1 --stop $s2
+  rm -fr unifrac.tmp
+  s1=$s2
+done
+t2=`date +%s`
+let dt=t2-t1
+echo "=== unweighted -f time: $dt ==="
+
+echo "=== unweighted fp32 100k/200k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+s1=0
+for ((i=0; $i<8; i+=1)); do
+  let s2=s1+12500
+  echo "`date` ssu --start $s1 --stop $s2"
+  taskset -c 0-7 time ssu -m unweighted_fp32 -i unifrac_200000.biom -t unifrac_200000.tre -o unifrac.tmp --mode partial --start $s1 --stop $s2
+  rm -fr unifrac.tmp
+  s1=$s2 
+done
+t2=`date +%s`
+let dt=t2-t1
+echo "=== unweighted fp32 time: $dt ==="
+
+t1=`date +%s`
+s1=0
+for ((i=0; $i<8; i+=1)); do
+  let s2=s1+12500
+  echo "`date` ssu --start $s1 --stop $s2"
+  taskset -c 0-7 time ssu -m unweighted_fp32 -f -i unifrac_200000.biom -t unifrac_200000.tre -o unifrac.tmp --mode partial --start $s1 --stop $s2
+  rm -fr unifrac.tmp
+  s1=$s2  
+done
+t2=`date +%s`
+let dt=t2-t1
+echo "=== unweighted fp32 -f time: $dt ==="
+
