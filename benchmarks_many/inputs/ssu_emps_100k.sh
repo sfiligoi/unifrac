51fc47a50b1d69117c07461a45f17371f5051aa8
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-02-28 13:22:28 -0800

    Add benchmarks_many/inputs

---
+++ benchmarks_many/inputs/ssu_emps_100k.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+for sz in 100; do 
+
+let szfull=1000*sz
+let stripes=500*sz
+
+echo "==== Size ${sz}k"
+
+for itr in 0 1 2 3 4 5 6 7 8 9; do
+
+fnameb=/home/mcdonadt/redbiom-070920/subsets/samples-${szfull}-iteration-${itr}
+
+echo "==== Processing ${fnameb}"
+echo "=== Size: ${szfull} Iteration: ${itr} ==="
+md5sum ${fnameb}.biom
+md5sum ${fnameb}.tre
+../../dev/t5/unifrac/sucpp/ssu -i ${fnameb}.biom -t ${fnameb}.tre -m unweighted_fp32 -o /dev/shm/unifrac.tmp --mode partial-report
+../../dev/t5/unifrac/sucpp/ssu -i ${fnameb}.biom -t ${fnameb}.tre -m unweighted_fp32 -o /dev/shm/unifrac.tmp --mode partial --start 0 --stop 1
+rm -f /dev/shm/unifrac.tmp
+
+done
+ 
+done
+
