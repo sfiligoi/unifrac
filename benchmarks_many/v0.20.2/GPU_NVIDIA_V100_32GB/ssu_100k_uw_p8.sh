5158fced2af0f009343812eb4221ae0d95bfa34e
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-02-28 13:27:37 -0800

    Add benchmarks_many/v0.20.2/GPU_NVIDIA_V100_32GB

---
+++ benchmarks_many/v0.20.2/GPU_NVIDIA_V100_32GB/ssu_100k_uw_p8.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --time 48:00:00 -p gpu --exclusive
+hostname
+lscpu
+date
+nvidia-smi
+
+export ACC_DEVICE_NUM=2
+export OMP_NUM_THREADS=8
+
+cd /panfs/panfs1.ucsd.edu/panscratch/isfiligoi/anaconda3_2020.11_20.2/benchmarks_many/ssu_gpu
+source ../../setup_conda.source
+conda activate unifrac-gpu
+
+for sz in 100; do 
+
+let szfull=1000*sz
+let stripes=500*sz
+
+let stripes1=stripes/8
+
+
+echo "==== Size ${sz}k"
+
+for itr in 0 1 2 3 4 5 6 7 8 9; do
+
+fnameb=/home/mcdonadt/redbiom-070920/subsets/samples-${szfull}-iteration-${itr}
+
+echo "==== Processing ${fnameb}"
+md5sum ${fnameb}.biom
+md5sum ${fnameb}.tre
+ssu -i ${fnameb}.biom -t ${fnameb}.tre -m unweighted -o /dev/shm/unifrac.tmp --mode partial-report
+
+echo "==== Size: ${sz}k Iteration: ${itr} Type: unweighted"
+rm -fr /dev/shm/unifrac.tmp
+t1=`date +%s`
+for n in 0 1 2 3 4 5 6 7; do
+let sstart=n*stripes1
+let n2=n+1
+let send=n2*stripes1
+
+taskset -c 8-15 time ssu -m unweighted -i ${fnameb}.biom -t ${fnameb}.tre -o /dev/shm/unifrac.tmp --mode partial --start ${sstart} --stop ${send}
+rm -fr /dev/shm/unifrac.tmp
+
+done
+t2=`date +%s`
+let dt=t2-t1
+echo "=== Size: ${szfull} Iteration: ${itr} Type: unweighted time: $dt ==="
+
+done
+
+done
---
+++ benchmarks_many/v0.20.2/GPU_NVIDIA_V100_32GB/ssu_1k_5k.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --time 24:00:00 -p gpu --exclusive
+hostname
+lscpu
+date
+nvidia-smi
+
+export ACC_DEVICE_NUM=0
+export OMP_NUM_THREADS=8
+
+cd /panfs/panfs1.ucsd.edu/panscratch/isfiligoi/anaconda3_2020.11_20.2/benchmarks_many/ssu_gpu
+source ../../setup_conda.source
+conda activate unifrac-gpu
+
+for sz in 1 2 5; do 
+
+let szfull=1000*sz
+let stripes=500*sz
+
+echo "==== Size ${sz}k"
+
+for itr in 0 1 2 3 4 5 6 7 8 9; do
+
+fnameb=/panfs/panfs1.ucsd.edu/panscratch/isfiligoi/dev/t8/inputs/new/samples-${szfull}-iteration-${itr}
+
+echo "==== Processing ${fnameb}"
+md5sum ${fnameb}.biom
+md5sum ${fnameb}.tre
+ssu -i ${fnameb}.biom -t ${fnameb}.tre -m unweighted -o /dev/shm/unifrac.tmp --mode partial-report
+
+echo "==== Size: ${sz}k Iteration: ${itr} Type: unweighted"
+rm -fr /dev/shm/unifrac.tmp
+t1=`date +%s`
+taskset -c 0-7 time ssu -m unweighted -i ${fnameb}.biom -t ${fnameb}.tre -o /dev/shm/unifrac.tmp --mode partial --start 0 --stop ${stripes}
+t2=`date +%s`
+let dt=t2-t1
+rm -fr /dev/shm/unifrac.tmp
+echo "=== Size: ${szfull} Iteration: ${itr} Type: unweighted time: $dt ==="
+
+echo "==== Size: ${sz}k Iteration: ${itr} Type: weighted_normalized"
+rm -fr /dev/shm/unifrac.tmp
+t1=`date +%s`
+taskset -c 0-7 time ssu -m weighted_normalized -i ${fnameb}.biom -t ${fnameb}.tre -o /dev/shm/unifrac.tmp --mode partial --start 0 --stop ${stripes}
+t2=`date +%s`
+let dt=t2-t1
+rm -fr /dev/shm/unifrac.tmp
+echo "=== Size: ${szfull} Iteration: ${itr} Type: weighted_normalized time: $dt ==="
+
+done
+
+done
