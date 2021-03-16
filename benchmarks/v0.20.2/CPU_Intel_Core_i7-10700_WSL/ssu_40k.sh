226c46f06b5422272eb115b333cedddc6df37e06
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-03-16 08:04:00 -0700

    More benachmark results

---
+++ benchmarks/v0.20.2/CPU_Intel_Core_i7-10700_WSL/ssu_40k.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+hostname
+lscpu
+date
+
+export OMP_NUM_THREADS=8
+
+source ~/setup_conda.source 
+conda activate unifrac-202
+
+md5sum ../unifrac_40000.biom
+md5sum ../unifrac_40000.tre
+
+echo "===="
+ssu -i ../unifrac_40000.biom -t ../unifrac_40000.tre -m unweighted -o unifrac.tmp --mode partial-report
+echo "=== unweighted 20k/40k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 0-7 time ssu -m unweighted -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 5000 --stop 10000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 10000 --stop 15000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 15000 --stop 20000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted time: $dt ==="
+
+t1=`date +%s`
+taskset -c 0-7 time ssu -m unweighted -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 5000 --stop 10000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 10000 --stop 15000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 15000 --stop 20000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted -f time: $dt ==="
+
+echo "=== unweighted fp32 20k/40k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 0-7 time ssu -m unweighted_fp32 -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted_fp32 -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 5000 --stop 10000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted_fp32 -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 10000 --stop 15000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted_fp32 -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 15000 --stop 20000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted fp32 time: $dt ==="
+
+t1=`date +%s`
+taskset -c 0-7 time ssu -m unweighted_fp32 -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted_fp32 -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 5000 --stop 10000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted_fp32 -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 10000 --stop 15000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m unweighted_fp32 -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 15000 --stop 20000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted fp32 -f time: $dt ==="
+
+echo "=== weighted_normalized 20k/40k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 0-7 time ssu -m weighted_normalized -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 5000 --stop 10000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 10000 --stop 15000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 15000 --stop 20000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized time: $dt ==="
+
+t1=`date +%s`
+taskset -c 0-7 time ssu -m weighted_normalized -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 5000 --stop 10000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 10000 --stop 15000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 15000 --stop 20000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized -f time: $dt ==="
+
+echo "=== weighted_normalized fp32 20k/40k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 0-7 time ssu -m weighted_normalized_fp32 -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized_fp32 -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 5000 --stop 10000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized_fp32 -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 10000 --stop 15000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized_fp32 -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 15000 --stop 20000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized fp32 time: $dt ==="
+
+t1=`date +%s`
+taskset -c 0-7 time ssu -m weighted_normalized_fp32 -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized_fp32 -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 5000 --stop 10000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized_fp32 -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 10000 --stop 15000
+rm -fr unifrac.tmp
+taskset -c 0-7 time ssu -m weighted_normalized_fp32 -f -i ../unifrac_40000.biom -t ../unifrac_40000.tre -o unifrac.tmp --mode partial --start 15000 --stop 20000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized fp32 -f time: $dt ==="
+
