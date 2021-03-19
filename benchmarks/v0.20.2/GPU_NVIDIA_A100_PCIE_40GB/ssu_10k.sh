de75a3bf5323d1e3cf67fd907df65d2958b7aa0f
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-03-19 16:20:26 +0000

    Add benchmarks

---
+++ benchmarks/v0.20.2/GPU_NVIDIA_A100_PCIE_40GB/ssu_10k.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+hostname
+lscpu
+date
+nvidia-smi
+
+export ACC_DEVICE_NUM=0
+export OMP_NUM_THREADS=8
+
+source ~/.bashrc
+conda activate unifrac-gpu
+
+md5sum unifrac_10000.biom
+md5sum unifrac_10000.tre
+
+echo "===="
+ssu -i unifrac_10000.biom -t unifrac_10000.tre -m unweighted -o unifrac.tmp --mode partial-report
+echo "=== unweighted 5k/10k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 8-15 time ssu -m unweighted -i unifrac_10000.biom -t unifrac_10000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted time: $dt ==="
+
+t1=`date +%s`
+taskset -c 8-15 time ssu -m unweighted -f -i unifrac_10000.biom -t unifrac_10000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted -f time: $dt ==="
+
+echo "=== unweighted fp32 5k/10k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 8-15 time ssu -m unweighted_fp32 -i unifrac_10000.biom -t unifrac_10000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted fp32 time: $dt ==="
+
+t1=`date +%s`
+taskset -c 8-15 time ssu -m unweighted_fp32 -f -i unifrac_10000.biom -t unifrac_10000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted fp32 -f time: $dt ==="
+
+echo "=== weighted_normalized 5k/10k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 8-15 time ssu -m weighted_normalized -i unifrac_10000.biom -t unifrac_10000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized time: $dt ==="
+
+t1=`date +%s`
+taskset -c 8-15 time ssu -m weighted_normalized -f -i unifrac_10000.biom -t unifrac_10000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized -f time: $dt ==="
+
+
+echo "=== weighted_normalized fp32 5k/10k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 8-15 time ssu -m weighted_normalized_fp32 -i unifrac_10000.biom -t unifrac_10000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized fp32 time: $dt ==="
+
+t1=`date +%s`
+taskset -c 8-15 time ssu -m weighted_normalized_fp32 -f -i unifrac_10000.biom -t unifrac_10000.tre -o unifrac.tmp --mode partial --start 0 --stop 5000
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized fp32 -f time: $dt ==="
+
---
+++ benchmarks/v0.20.2/GPU_NVIDIA_A100_PCIE_40GB/ssu_1k.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+hostname
+lscpu
+date
+nvidia-smi
+
+export ACC_DEVICE_NUM=0
+export OMP_NUM_THREADS=8
+
+source ~/.bashrc 
+conda activate unifrac-gpu
+
+md5sum unifrac_1000.biom
+md5sum unifrac_1000.tre
+
+echo "===="
+ssu -i unifrac_1000.biom -t unifrac_1000.tre -m unweighted -o unifrac.tmp --mode partial-report
+echo "=== unweighted 500/1k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 8-15 time ssu -m unweighted -i unifrac_1000.biom -t unifrac_1000.tre -o unifrac.tmp --mode partial --start 0 --stop 500
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted time: $dt ==="
+
+t1=`date +%s`
+taskset -c 8-15 time ssu -m unweighted -f -i unifrac_1000.biom -t unifrac_1000.tre -o unifrac.tmp --mode partial --start 0 --stop 500
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted -f time: $dt ==="
+
+echo "=== unweighted fp32 500/1k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 8-15 time ssu -m unweighted_fp32 -i unifrac_1000.biom -t unifrac_1000.tre -o unifrac.tmp --mode partial --start 0 --stop 500
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted fp32 time: $dt ==="
+
+t1=`date +%s`
+taskset -c 8-15 time ssu -m unweighted_fp32 -f -i unifrac_1000.biom -t unifrac_1000.tre -o unifrac.tmp --mode partial --start 0 --stop 500
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== unweighted fp32 -f time: $dt ==="
+
+echo "=== weighted_normalized 500/1k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 8-15 time ssu -m weighted_normalized -i unifrac_1000.biom -t unifrac_1000.tre -o unifrac.tmp --mode partial --start 0 --stop 500
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized time: $dt ==="
+
+t1=`date +%s`
+taskset -c 8-15 time ssu -m weighted_normalized -f -i unifrac_1000.biom -t unifrac_1000.tre -o unifrac.tmp --mode partial --start 0 --stop 500
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized -f time: $dt ==="
+
+
+echo "=== weighted_normalized fp32 500/1k ==="
+rm -fr unifrac.tmp
+t1=`date +%s`
+taskset -c 8-15 time ssu -m weighted_normalized_fp32 -i unifrac_1000.biom -t unifrac_1000.tre -o unifrac.tmp --mode partial --start 0 --stop 500
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized fp32 time: $dt ==="
+
+t1=`date +%s`
+taskset -c 8-15 time ssu -m weighted_normalized_fp32 -f -i unifrac_1000.biom -t unifrac_1000.tre -o unifrac.tmp --mode partial --start 0 --stop 500
+t2=`date +%s`
+let dt=t2-t1
+rm -fr unifrac.tmp
+echo "=== weighted_normalized fp32 -f time: $dt ==="
+
